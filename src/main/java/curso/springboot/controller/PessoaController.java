75f83aab277844620b8ef1faad25a0f09ece8205
Matheus Sousa <matheus.ads.study@gmail.com>
2019-09-23 08:57:19 -0300

    # 06 - Carregando nossa lista de pessoas cadastradas

--- src/main/java/curso/springboot/controller/PessoaController.java
+++ src/main/java/curso/springboot/controller/PessoaController.java
@@ -4,25 +4,35 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 import curso.springboot.model.Pessoa;
 import curso.springboot.repository.PessoaRepository;
 
 @Controller
 public class PessoaController {
-	
+
 	@Autowired
 	private PessoaRepository pessoaRepository;
-	
-	@RequestMapping( method = RequestMethod.GET, value="/cadastropessoa" ) 
+
+	@RequestMapping(method = RequestMethod.GET, value = "/cadastropessoa")
 	public String incio() {
 		return "cadastro/cadastropessoa";
 	}
-	
-	@RequestMapping( method = RequestMethod.POST, value="/salvarpessoa")
-    public String salvar( Pessoa pessoa ) {
+
+	@RequestMapping(method = RequestMethod.POST, value = "/salvarpessoa")
+	public String salvar(Pessoa pessoa) {
 		pessoaRepository.save(pessoa);
-    	return "cadastro/cadastropessoa";
-    }
+		return "cadastro/cadastropessoa";
+	}
+
+	@RequestMapping(method = RequestMethod.GET, value = "/listapessoas")
+	public ModelAndView pessoas() {
+
+		ModelAndView modelAndView = new ModelAndView("cadastro/cadastropessoa");
+		Iterable<Pessoa> pessoasItr = pessoaRepository.findAll();
+		modelAndView.addObject("pessoas", pessoasItr);
+		return modelAndView;
+	}
 
 }
