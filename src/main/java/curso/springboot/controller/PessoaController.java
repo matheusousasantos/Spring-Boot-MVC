b07314b6c0d06bb211e80302a2eedbe2640396c1
Matheus Sousa <matheus.ads.study@gmail.com>
2019-10-07 20:12:52 -0300

    # 18 - Realizando validações com Spring

--- src/main/java/curso/springboot/controller/PessoaController.java
+++ src/main/java/curso/springboot/controller/PessoaController.java
@@ -1,9 +1,15 @@
 package curso.springboot.controller;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
+
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -38,7 +44,27 @@ public class PessoaController {
 	}
 
 	@RequestMapping(method = RequestMethod.POST, value = "**/salvarpessoa")
-	public ModelAndView salvar(Pessoa pessoa) {
+	public ModelAndView salvar(@Valid Pessoa pessoa, BindingResult bindingResult) {
+		
+		
+		if(bindingResult.hasErrors()) {
+			
+			ModelAndView modelAndView = new ModelAndView("cadastro/cadastropessoa");
+			Iterable<Pessoa> pessoasItr = pessoaRepository.findAll();
+			modelAndView.addObject("pessoaobj", pessoa);
+			modelAndView.addObject("pessoas", pessoasItr);
+			
+			List<String> msg = new ArrayList<String>();
+			
+			for(ObjectError objectError : bindingResult.getAllErrors()) {
+				msg.add(objectError.getDefaultMessage()); //Vem das anotações de Validação do Pessoa
+			}
+			
+			modelAndView.addObject("msg", msg);
+			return modelAndView;
+			
+		}
+		
 		pessoaRepository.save(pessoa);
 
 		ModelAndView modelAndView = new ModelAndView("cadastro/cadastropessoa");
--- src/main/java/curso/springboot/model/Pessoa.java
+++ src/main/java/curso/springboot/model/Pessoa.java
@@ -10,6 +10,9 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 
 @Entity
 public class Pessoa implements Serializable {
@@ -20,13 +23,18 @@ public class Pessoa implements Serializable {
 	@GeneratedValue( strategy = GenerationType.AUTO )
 	private Long id;
 	
+	@NotEmpty( message = "Nome não pode ser Vazio!" )
+	@NotNull( message = "Nome não pode ser nulo!" )
 	private String nome;
 	
+	@NotEmpty( message = "Sobrenome não pode ser Vazio!" )
+	@NotNull( message = "Sobrenome não pode ser nulo!" )
 	private String sobrenome;
 	
 	@OneToMany( mappedBy = "pessoa", orphanRemoval = true, cascade = CascadeType.ALL )
 	List<Telefone> telefones = new ArrayList<Telefone>();
 	
+	@Min( value = 18, message = "Idade Invalida!" )
 	private int idade;
 
 	public Long getId() {
